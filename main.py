43ed139a2ad8ae4d13adfa9750bad57e14892f7a
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-09-18 23:45:47 +0900

    "Tab 1 Completed"

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import json
 from pytimekr import pytimekr
 from datetime import datetime, timedelta
-
+import os
 
 def read_data(filename):
-    f = open('data/' + filename + '.json')
+    f = open('data/' + filename)
     output = json.load(f)
     f.close()
     return output
@@ -31,10 +31,15 @@ def list_dates(start, end):
         [date.strftime('%Y-%m-%d') + ' 점심' for date in dates] + [date.strftime('%Y-%m-%d') + ' 저녁' for date in dates])
 
 
-data = read_data('admin')
+data = read_data('admin.json')
+p_data = {}
+for f in os.listdir("data"):
+    if f != 'admin.json':
+        p_data[f.split('.')[0]] = read_data(f)
+
 is_name_selected = False
 
-st.title('회식 선정기')
+st.title('회식 투표기')
 
 tab1, tab2, tab3 = st.tabs(["회식 불가일 및 추천 장소 등록", "투표", "결과"])
 
@@ -48,25 +53,36 @@ with tab1:
         st.write(name + '님 환영합니다')
         is_name_selected = True
         if is_name_selected:
-            p_data = read_data(name)
+            p_datum = p_data[name]
 
             start_date = datetime.strptime(data['start_date'], '%Y-%m-%d')
             end_date = datetime.strptime(data['end_date'], '%Y-%m-%d')
-            unavail_dates = p_data['unavail_dates']
+            unavail_dates = p_datum['unavail_dates']
 
             options = st.multiselect(
                 data['start_date'] + " - " + data['end_date'] + " 사이의 기간 중 불가능한 날짜를 등록 해 주세요",
                 list_dates(start_date, end_date),
                 unavail_dates)
 
-        if st.button("등록"):
-            p_data["unavail_dates"] = options
-            p_data["registered"] = True
-            save_data(p_data, name)
+        if st.button("등록", "등록1"):
+            p_datum["unavail_dates"] = options
+            p_datum["registered"] = True
+            save_data(p_datum, name)
+            st.write('등록 완료!')
+
+        if is_name_selected:
+            place = st.text_input('장소를 등록 해 주세요', '')
+            st.write('현재 등록된 장소:', ", ".join(list(dict.fromkeys([item for sublist in [p_data[p]['locations'] for p in p_data] for item in sublist]))))
+
+        if st.button("등록", "등록2"):
+            p_datum["locations"] = p_datum["locations"] + [place]
+            p_datum["registered"] = True
+            save_data(p_datum, name)
             st.write('등록 완료!')
 
 with tab2:
     st.write('감사합니다')
+
 
 with tab3:
     st.write('감사합니다')
